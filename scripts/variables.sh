8002cc7473f2e204ee8920ac9eb99e16b0c32cc4
Brent Holden <brent.holden@gmail.com>
2021-02-15 23:42:25 -0500

    changed backup service directories

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -6,16 +6,10 @@ declare -A SERVICES=( [pihole]=docker.io/pihole/pihole:latest
                       [netbootxyz]=docker.io/linuxserver/netbootxyz:latest
                       [unifi]=docker.io/linuxserver/unifi-controller:latest
                     )
-#declare -A BACKUPS=(  [lidarr]=/opt/lidarr/Backups/scheduled
-#                      [sonarr]=/opt/sonarr/Backups/scheduled
-#                      [radarr]=/opt/radarr/Backups/scheduled
-#                      [tautulli]=/opt/tautulli/backups
-#                      [hydra2]=/opt/hydra2/backup
-#                      [sabnzbd]=/opt/sabnzbd/
-#                      [ombi]=/opt/ombi/
-#                      [caddy]=/opt/caddy/
-#                      [plex]=/opt/plex/Backups
-#                    )
+declare -A BACKUPS=(  [pihole]=/opt/pihole
+                      [netbootxyz]=/opt/netbootxyz
+                      [unifi]=/opt/unifi
+                    )
 DATE=`date +%d-%m-%Y`
 CRONDIR=/etc/cron.d
 OPTDIR=/opt
