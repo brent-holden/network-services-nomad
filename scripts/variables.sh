3cb0c6be403f5dad17e70f2af7eef9a5126e586e
Brent Holden <brent.holden@gmail.com>
2021-02-23 15:17:42 -0500

    Added tfc-agent

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-#SERVICES=(pihole netbootxyz unifi)
-
 declare -A SERVICES=( [pihole]=docker.io/pihole/pihole:latest
                       [netbootxyz]=docker.io/linuxserver/netbootxyz:latest
                       [unifi]=docker.io/linuxserver/unifi-controller:latest
+                      [tfc-agent]=docker.io/hashicorp/tfc-agent:latest
                     )
+
 declare -A BACKUPS=(  [pihole]=/opt/pihole
                       [netbootxyz]=/opt/netbootxyz
                       [unifi]=/opt/unifi
+                      [tfc-agent]=/dev/null
                     )
+
 DATE=`date +%d-%m-%Y`
 CRONDIR=/etc/cron.d
 OPTDIR=/opt
@@ -18,5 +20,5 @@ RCLONEDIR=/mnt/rclone
 RCLONECACHEDIR=${RCLONEDIR}/cache-db
 RCLONEBACKUPDIR=${RCLONEDIR}/backup
 RCLONECONFIGDIR=${OPTDIR}/rclone
-PACKAGES="fuse rsync vim podman podman-docker podman-remote cockpit cockpit-podman"
+PACKAGES="fuse rsync vim podman podman-docker cockpit cockpit-podman"
 NOMAD_PLUGIN_DIR=/opt/nomad/plugins
